378938cd6b985a63ff7cc93751829613a7ceecab
natrix187 <natrix.871@gmail.com>
2025-08-20 11:29:03 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot Discord en ligne !'));
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Serveur web lancé sur le port ${process.env.PORT || 3000}`);
+});
+
 require("dotenv").config();
 require("module-alias/register");
 
